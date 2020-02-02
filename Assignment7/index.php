c11590b44b1fdd4d0ffcc07823a962cb7ec9d196
dkkondo <58612684+dkkondo@users.noreply.github.com>
2020-02-02 14:54:15 -0600

    Create index.php
    assignment7

---
+++ Assignment7/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       
+  <title>Assignment 7</title>
+  <meta charset="utf-8"> 
+</head>
+<body>
+	 <!-- Dissa Kondo
+     index.php
+     INFO1311.WW
+     Lisa THOENDEL 
+     2/1/20
+-->
+
+<?php
+  $my_array = array("Food", "Value", "Place", "Animal");
+  $elements = count($my_array);
+?>
+
+<h1>Good morning, Nebraska!</h1>
+<div>Last night I ate <?php echo "rice"; ?>.</div>
+<div>While it was good, it wasn't perfect. I'd give it <?php echo "2"; ?> stars out of 4.</div>
+<div>Tomorrow I plan to go to the <?php echo "mall"; ?>.</div>
+<div>My favorite animal is the <?php echo "dog"; ?>.</div>
+
+</body>
+</html>
